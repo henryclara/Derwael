bfef44d6d8ad89703c71a918fe17ca5564e3fb09
Clara Henry <clara.henry@mpimet.mpg.de>
2022-04-08 09:58:28 +0200

    Create a directory from which the simulation will be run

--- Forward/RunForward.sh
+++ Forward/RunForward.sh
@@ -6,24 +6,32 @@
 Name=SimName
 
 # Create a directory to store model output
-mkdir -p Simulations/${Name}
-cd Simulations/${Name}
+mkdir -p Simulations/${Name}/Mesh
+cp Forward.sif Simulations/${Name}
+cp Submit.sh Simulations/${Name}
+cp Derwael2.nc Simulations/${Name}
+cp SMB_Derval2.nc Simulations/${Name}
+cp ModulesPlusPathsMistralGCC71.sh Simulations/${Name}
+cp -r src Simulations/${Name}
+
+
+
 
 # Set up a counter for keeping track of the number of outputs
-Counter=$1
+#Counter=$1
 
-while [ "${Counter}" -lt "1" ]; do
+#while [ "${Counter}" -lt "1" ]; do
 
 # This line creates a directory for the first set of output simulations
-mkdir -p Output$(printf "%06d" ${Counter} )	
-cd Output$(printf "%06d" ${Counter} )
-cp ../../../Submit.sh .
-cp ../../../Forward.sif .
+#mkdir -p Output$(printf "%06d" ${Counter} )	
+#cd Output$(printf "%06d" ${Counter} )
+#cp ../../../Submit.sh .
+#cp ../../../Forward.sif .
 
-sbatch Submit.sh ${Counter}
+#sbatch Submit.sh ${Counter}
 
-Counter=$((Counter + 1))
+#Counter=$((Counter + 1))
 
-done
+#done
 
 
