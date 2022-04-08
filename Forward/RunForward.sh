00752b085a7a6abb97d942b212376ddcbeafda03
Clara Henry <clara.henry@mpimet.mpg.de>
2022-04-08 15:20:15 +0200

    Commit changes before setting up age solver

--- Forward/RunForward.sh
+++ Forward/RunForward.sh
@@ -7,33 +7,22 @@ Name=SimName
 Counter=$1
 
 # Create a directory to store model output
-mkdir -p Simulations/${Name}/{Mesh,Logs,Output$(printf "%06d" ${Counter})
-cp Mesh/{mesh*,partitioning.80,*result*} Simulations/${Name}/Mesh
+mkdir -p Simulations/${Name}/{Mesh,Logs,Output$(printf "%06d" ${Counter})}
+cp -r Mesh/{mesh*,partitioning.80,*result*} Simulations/${Name}/Mesh
 cp -r {Forward2.sif,Submit2.sh,Derwael2.nc,SMB_Derval2.nc,ModulesPlusPathsMistralGCC71.sh,Makefile,src} Simulations/${Name}
 
-cd Simulations/${Name} 
+cd Simulations/${Name}
+cp Mesh/*result* Output$(printf "%06d" ${Counter}) 
+
 sbatch Submit2.sh
 
+#while [[ ${Counter} -lt 2 ]]; do
 
-
-
-
-
-# Set up a counter for keeping track of the number of outputs
-#Counter=$1
-
-#while [ "${Counter}" -lt "1" ]; do
-
-# This line creates a directory for the first set of output simulations
-#mkdir -p Output$(printf "%06d" ${Counter} )	
-#cd Output$(printf "%06d" ${Counter} )
-#cp ../../../Submit.sh .
-#cp ../../../Forward.sif .
-
-#sbatch Submit.sh ${Counter}
-
-#Counter=$((Counter + 1))
-
+#	mkdir -p Output$(printf "%06d" ${Counter})
+#	sbatch Submit2.sh
+#	cp -r Mesh/*result* Output$(printf "%06d" ${Counter})
+#	mv Mesh/*vtu Output$(printf "%06d" ${Counter})
+#	Counter=$((Counter + 1))
+	
 #done
-
 
