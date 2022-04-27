4dfa7c9f1ec5c4ff5d30e00a84478388290fad3b
Clara Henry <clara.henry@mpimet.mpg.de>
2022-04-27 12:11:44 +0200

    Change files created etc

--- Forward/RunForward.sh
+++ Forward/RunForward.sh
@@ -3,26 +3,15 @@
 # Run this script to create a new simulation
 
 # Indicate a name for the new simulation
-Name=SimName
-Counter=$1
+Name=$1
 
 # Create a directory to store model output
-mkdir -p Simulations/${Name}/{Mesh,Logs,Output$(printf "%06d" ${Counter})}
-cp -r Mesh/{mesh*,partitioning.80,*result*} Simulations/${Name}/Mesh
-cp -r {Forward2.sif,Submit2.sh,Derwael2.nc,SMB_Derval2.nc,ModulesPlusPathsMistralGCC71.sh,Makefile,src} Simulations/${Name}
+mkdir -p Simulations/${Name}/{Mesh,Logs}
+cp -r Mesh/{mesh*,partitioning.80,Forward000000.result.*} Simulations/${Name}/Mesh
+cp -r {Forward.sif,Forward.sif.bak,Submit.sh,Submit0.sh,Derwael2.nc,SMB_Derval2.nc,ModulesPlusPathsMistralGCC71.sh,Makefile,src} Simulations/${Name}
 
 cd Simulations/${Name}
 cp Mesh/*result* Output$(printf "%06d" ${Counter}) 
 
-sbatch Submit2.sh
-
-#while [[ ${Counter} -lt 2 ]]; do
-
-#	mkdir -p Output$(printf "%06d" ${Counter})
-#	sbatch Submit2.sh
-#	cp -r Mesh/*result* Output$(printf "%06d" ${Counter})
-#	mv Mesh/*vtu Output$(printf "%06d" ${Counter})
-#	Counter=$((Counter + 1))
-	
-#done
+sbatch Submit0.sh
 
