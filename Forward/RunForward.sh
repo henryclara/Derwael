fb1c2a00e36c9c938a56363bc39125c3ed713582
Clara Henry <clara.henry@mpimet.mpg.de>
2022-04-29 16:19:52 +0200

    Update file names

--- Forward/RunForward.sh
+++ Forward/RunForward.sh
@@ -8,7 +8,7 @@ Name=$1
 # Create a directory to store model output
 mkdir -p Simulations/${Name}/{Mesh,Logs}
 cp -r Mesh/{mesh*,partitioning.80,Forward000000.result.*} Simulations/${Name}/Mesh
-cp -r {Forward.sif,Forward.sif.bak,Submit.sh,Submit0.sh,Derwael.nc,SMB_Derval2.nc,ModulesPlusPathsMistralGCC71.sh,Makefile,src} Simulations/${Name}
+cp -r {Forward.sif,Forward.sif.bak,Submit.sh,Submit0.sh,Derwael.nc,Derwael_SMB.nc,ModulesPlusPathsMistralGCC71.sh,Makefile,src} Simulations/${Name}
 
 cd Simulations/${Name}
 
