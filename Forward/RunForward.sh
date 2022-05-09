3bd86649c31b7913e84bffc5f70e6ca77c7da4b0
Clara Henry <clara.henry@mpimet.mpg.de>
2022-05-09 15:45:26 +0200

    Add temp solver and netcdf file

--- Forward/RunForward.sh
+++ Forward/RunForward.sh
@@ -8,7 +8,7 @@ Name=$1
 # Create a directory to store model output
 mkdir -p Simulations/${Name}/{Mesh,Logs}
 cp -r Mesh/{mesh*,partitioning.80,Forward000000.result.*} Simulations/${Name}/Mesh
-cp -r {Forward.sif,Forward.sif.bak,Submit.sh,Submit0.sh,Derwael.nc,Derwael_SMB.nc,Derwael_BMB.nc,Derwael_BMB_Test.nc,ModulesPlusPathsMistralGCC71.sh,Makefile,src} Simulations/${Name}
+cp -r {Forward.sif,Forward.sif.bak,Submit.sh,Submit0.sh,Derwael.nc,Derwael_SMB.nc,Derwael_BMB.nc,Derwael_BMB_Test.nc,Derwael_SurfTemp.nc,ModulesPlusPathsMistralGCC71.sh,Makefile,src} Simulations/${Name}
 
 cd Simulations/${Name}
 
