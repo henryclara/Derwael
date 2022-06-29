90d0f28bb347d5b60f46dd6366e83ae0834fe8b7
Clara Henry <clara.henry@mpimet.mpg.de>
2022-06-29 12:01:26 +0200

    Add copying SMB ncs multiplied by specified factors

--- Forward/RunForward.sh
+++ Forward/RunForward.sh
@@ -14,7 +14,7 @@ Name=$1
 # Create a directory to store model output
 mkdir -p Simulations/${Name}/{Mesh,Logs}
 cp -r ../Init/Mesh/{mesh*,partitioning.80,Forward000000.result.*} Simulations/${Name}/Mesh
-cp -r {Forward.sif.bak,Submit.sh,Submit0.sh,DerwaelBCs.nc,Derwael_SMB.nc,Derwael_SMB_Smoothed.nc,Derwael_BMB.nc,Derwael_SurfTemp.nc,ModulesPlusPaths2LoadIntelMPI.sh,Makefile,src} Simulations/${Name}
+cp -r {Forward.sif.bak,Submit.sh,Submit0.sh,DerwaelBCs.nc,Derwael_SMB.nc,Derwael_SMB_Smoothed*.nc,Derwael_BMB.nc,Derwael_SurfTemp.nc,ModulesPlusPaths2LoadIntelMPI.sh,Makefile,src} Simulations/${Name}
 
 cd Simulations/${Name}
 
